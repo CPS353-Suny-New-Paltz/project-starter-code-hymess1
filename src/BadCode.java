4ad992de00250ba0d4000e26e5a981276ba533b0
hymess1 <115047247+hymess1@users.noreply.github.com>
2025-09-03 22:50:47 -0400

    Update BadCode.java
    should pass gradle and checkstyle

--- src/BadCode.java
+++ src/BadCode.java
@@ -1,8 +1,8 @@
 public class BadCode {
 	
 	public static void main(String[] args) {
-		int x,y = 4;
-		System.out.println("Hello World!") //Added semicolon &  space after comma
+		int x, y = 4;
+		System.out.println("Hello World!"); //Added semicolon &  space after comma
 		
 	}
 	
