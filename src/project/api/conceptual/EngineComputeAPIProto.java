9b23826e82510cf40d81773a69013e2c9932d3e2
hymess1 <hymess1@newpaltz.edu>
2025-10-05 19:13:36 -0400

    Fix Checkstyle: add braces and expand getters
    Added {} to all if/while and split one line getters so braces are on their own lines to satisfy NeedBraces and Curly rules.

--- src/project/api/conceptual/EngineComputeAPIProto.java
+++ src/project/api/conceptual/EngineComputeAPIProto.java
@@ -29,10 +29,16 @@ public class EngineComputeAPIProto {
 
     
     private boolean isPrime(int x) {
-        if (x < 2) return false;
-        if (x % 2 == 0) return x == 2;
+        if (x < 2) {
+        	return false;
+        }
+        if (x % 2 == 0) {
+        	return x == 2;
+        }
         for (int i = 3; i * i <= x; i += 2) {
-            if (x % i == 0) return false;
+            if (x % i == 0) {
+            	return false;
+            }
         }
         return true;
     }
