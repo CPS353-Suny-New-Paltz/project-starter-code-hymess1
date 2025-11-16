5e05edce0cd42672f833289b7eb16d8850e39e46
hymess1 <hymess1@newpaltz.edu>
2025-11-16 16:48:37 -0500

    Manual Testing Framework

--- test/project/checkpointtests/ManualTestingFramework.java
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -1,14 +1,19 @@
 package project.checkpointtests;
 
-import edu.softwareeng.sample.ComputationCoordinator;
-import edu.softwareeng.sample.ComputeEngine;
-import edu.softwareeng.sample.ComputeEngineImpl;
-import edu.softwareeng.sample.ComputeRequest;
-import edu.softwareeng.sample.CoordinatorImpl;
-import edu.softwareeng.sample.DataStore;
-import edu.softwareeng.sample.DataStoreImpl;
-import edu.softwareeng.sample.FileInputConfig;
-import edu.softwareeng.sample.FileOutputConfig;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import project.api.conceptual.EngineComputeAPI;
+import project.api.conceptual.EngineComputeAPIImpl;
+import project.api.network.NetworkService;
+import project.api.network.NetworkServiceImpl;
+import project.api.process.DataIOService;
+import project.api.process.DataIOServiceImpl;
+import project.model.DelimiterSpec;
+import project.model.JobRequest;
+import project.model.JobResult;
 
 public class ManualTestingFramework {
     
@@ -20,18 +25,28 @@ public class ManualTestingFramework {
         // Instantiate a real (ie, class definition lives in the src/ folder) implementation 
         // of all 3 APIs
         
-        NetworkService network = new NetworkServiceImpl();
-        DataIOService dataIO = new DataIOServiceImpl();
-        EngineComputeAPI engine = new EngineComputeAPIImpl();
+    	 DataIOService dataIO = new DataIOServiceImpl();
+         EngineComputeAPI engine = new EngineComputeAPIImpl();
+         NetworkService network = new NetworkServiceImpl(dataIO, engine);
+      // make sure the output file starts empty for each run 
+         Path outputPath = Paths.get(OUTPUT);
+         try {
+             Files.deleteIfExists(outputPath);
+         } catch (IOException e) {
+             // Manual harness: ignore delete failures, just try to continue
+         }
         // TODO 2:
         // Run a computation with an input file of <root project dir>/manualTestInput.txt
         // and an output of <root project dir>/manualTestOutput.txt, with a delimiter of ',' 
-        ComputationCoordinator coordinator =
-                new CoordinatorImpl(network, dataIO, engine);
+         DelimiterSpec delims = new DelimiterSpec(false, ",", ":");
 
-            FileInputConfig inputCfg = new FileInputConfig(INPUT, ",");
-            FileOutputConfig outputCfg = new FileOutputConfig(OUTPUT);
-        //
+         JobRequest job = new JobRequest(INPUT, OUTPUT, delims);
+
+         JobResult result = network.submitJob(job);
+         
+         if (!result.isSuccess()) {
+             String ignore = result.getErrorMessage();
+         }
         // Helpful hint: the working directory of this program is <root project dir>,
         // so you can refer to the files just using the INPUT/OUTPUT constants. You do not 
         // need to (and should not) actually create those files in your repo
